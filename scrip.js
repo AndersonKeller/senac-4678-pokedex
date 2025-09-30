093841969a3938c3f333592e56f5f3dac0a0a28c
AndersonKeller <andersonkellervendas@hotmail.com>
2025-09-29 21:10:35 -0300

    add abilities for each

--- scrip.js
+++ scrip.js
@@ -20,8 +20,23 @@ console.log("rodou função")
         console.log(infos,"infos")
         const img = document.createElement("img")
         img.src = infos.sprites.front_default
-       
-        li.append(img,p)
+       //expeirence
+        const experience = document.createElement("p")
+        experience.innerText ="Base experience: " + infos.base_experience
+
+        //abilities
+        const abilities = document.createElement("div")
+        const abilityTitle = document.createElement("p")
+        abilityTitle.innerText = "Abilities"
+        abilities.append(abilityTitle)
+        infos.abilities.forEach((item)=>{
+    
+            const ability = document.createElement("p")
+            ability.innerText = item.ability.name
+            abilities.append(ability)
+        })
+        
+        li.append(img,p,experience,abilities)
         ul.append(li)
     })
 }
