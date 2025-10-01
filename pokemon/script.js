9c927132298dd28635fb20177fee1f187758f064
AndersonKeller <andersonkellervendas@hotmail.com>
2025-10-01 19:51:44 -0300

    feat: add name pokemon page

--- pokemon/script.js
+++ pokemon/script.js
@@ -2,5 +2,9 @@
 function getPokemon(){
     const infos = JSON.parse(localStorage.getItem("pokemon"))
     console.log(infos, "pokemon")
+    const div = document.querySelector("div")
+    const p = document.createElement("p")
+    p.innerText = infos.name
+    div.append(p)
 }
 getPokemon()
